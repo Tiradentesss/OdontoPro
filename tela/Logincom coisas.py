e97a73c28e9788f1dc7cee490d4cb4f69d73bba7
Celiosss <celio56430576@edu.pa.senac.br>
2026-01-21 16:08:41 -0300

    update

--- tela/Logincom coisas.py
+++ tela/Logincom coisas.py
@@ -27,3 +27,33 @@ def tela_login():
 
     container = ctk.CTkFrame(app, corner_radius=20)
     container.pack(expand=True, fill="both", padx=40, pady=40)
+
+    container.grid_columnconfigure((0, 1), weight=1)
+    container.grid_rowconfigure(0, weight=1)
+
+    left = ctk.CTkFrame(container, fg_color="#1f6aa5", corner_radius=20)
+    left.grid(row=0, column=0, sticky="nsew")
+
+    ctk.CTkLabel(left, text="Bem Vindo!", font=("Arial", 34, "bold"), text_color="white").pack(pady=(120, 10))
+
+    ctk.CTkLabel(left, text="Sistema Desktop/Profissional", font=("Arial", 18), text_color="white").pack()
+
+    ctk.CTkLabel(left, text="Faça login para continuar", font=("Arial", 14), text_color="white").pack(pady=(10))
+
+    right = ctk.CTkFrame(container, fg_color="white", corner_radius=20)
+    right.grid(row=0, column=1, sticky="nsew")
+
+    ctk.CTkLabel(left, text="LOGIN", font=("Arial", 30, "bold"), text_color="#1f6aa5").pack(pady=(120, 10))
+
+    usuario=ctk.CTkEntry(right, placeholder_text="Usuário", width=280, heigth=45)
+    usuario.pack(pady=10)
+
+    senha_frame=ctk.CTkFrame(right, fg_color="transparent")
+    senha_frame.pack(pady=10)
+
+    senha=ctk.CTkEntry(senha_frame, placeholder_text="Senha", show="*", width=280, heigth=45)
+    senha.pack(side="left", padx=(0, 5))
+
+    def autenticar():
+        user = usuario.get()
+        pwd = senha.get()
