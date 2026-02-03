9ff2be1202518a343409f1a29d490d9bf353c8db
Celiosss <celio56430576@edu.pa.senac.br>
2026-02-03 15:15:56 -0300

    mudar lugar

--- SistemaDesktop/views/base.py
+++ SistemaDesktop/views/base.py
@@ -1,14 +1,28 @@
 import customtkinter as ctk
+from views.theme import COLORS, FONTS
 
 class BaseScreen(ctk.CTkFrame):
     def __init__(self, parent, title):
-        super().__init__(parent, fg_color="transparent")
+        super().__init__(parent, fg_color=COLORS["bg"])
+
+        header = ctk.CTkFrame(self, fg_color="transparent")
+        header.pack(fill="x", padx=30, pady=(25, 10))
 
         ctk.CTkLabel(
-            self,
+            header,
             text=title,
-            font=ctk.CTkFont(size=28, weight="bold")
-        ).pack(anchor="w", pady=(0, 20))
+            font=FONTS["title"],
+            text_color=COLORS["text"]
+        ).pack(anchor="w")
 
-        self.content_card = ctk.CTkFrame(self, corner_radius=18)
-        self.content_card.pack(expand=True, fill="both")
+        self.content_card = ctk.CTkFrame(
+            self,
+            fg_color=COLORS["card"],
+            corner_radius=18
+        )
+        self.content_card.pack(
+            expand=True,
+            fill="both",
+            padx=30,
+            pady=20
+        )
