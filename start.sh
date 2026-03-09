fad349bb79ab82c6398365a77ec04bd83b958ebe
Celiosss <celio56430576@edu.pa.senac.br>
2026-03-09 17:27:30 -0300

    port upd

--- start.sh
+++ start.sh
@@ -2,8 +2,11 @@
 set -e
 
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 gunicorn setup.asgi:application \
   -k uvicorn.workers.UvicornWorker \
   --bind 0.0.0.0:$PORT
+  --workers 3 \               # <- adicione isso (ajuste conforme CPU/memória do plano)
+  --timeout 120 \             # <- útil se tiver tarefas longas
+  --log-level info
