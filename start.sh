8796de83b64afa2bc0f9f91a5f13fe267b69786a
Patrick Nascimento <pmoraes230nascimento@gmail.com>
2026-03-11 14:34:34 -0300

    fix migrations

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ python manage.py collectstatic --noinput
 
 gunicorn setup.wsgi:application \
   --bind 0.0.0.0:8000 \
-  --workers 4 \              # ajuste conforme sua CPU (regra comum: 2×núcleos + 1)
-  --timeout 120 \            # opcional, mas útil em produção
-  --access-logfile "-" \     # log no stdout (útil em docker/railway/fly/etc)
-  --error-logfile "-"        # log no stdout
+  --workers 4 \
+  --timeout 120 \
+  --access-logfile "-" \
+  --error-logfile "-"
