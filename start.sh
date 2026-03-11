5f5adb2faf4c8465dbde56880431645b9e682285
Patrick Nascimento <pmoraes230nascimento@gmail.com>
2026-03-11 14:21:21 -0300

    correção em porta start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 gunicorn setup.wsgi:application \
-  --bind 0.0.0.0:$PORT \
+  --bind 0.0.0.0:8000 \
   --workers 4 \              # ajuste conforme sua CPU (regra comum: 2×núcleos + 1)
   --timeout 120 \            # opcional, mas útil em produção
   --access-logfile "-" \     # log no stdout (útil em docker/railway/fly/etc)
