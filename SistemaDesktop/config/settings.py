ee699f8d76383e15b62a9f72fea0a50b962c58b3
Celiosss <celio56430576@edu.pa.senac.br>
2026-03-17 14:13:26 -0300

    opção de bancos

--- SistemaDesktop/config/settings.py
+++ SistemaDesktop/config/settings.py
@@ -2,11 +2,23 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+# Banco de dados Online (Aiven):
+
+# DB_CONFIG = {
+#     "host": "odontoplace-odontopro2-87cf.f.aivencloud.com",
+#     "user": "avnadmin",
+#     "password": os.getenv("DB_PASSWORD"),
+#     "database": "defaultdb",
+#     "port": 23912,
+#     "ssl_ca": "config/ca.pem"
+# }
+
+# Banco de dados Local :
+
 DB_CONFIG = {
-    "host": "odontoplace-odontopro2-87cf.f.aivencloud.com",
-    "user": "avnadmin",
-    "password": os.getenv("DB_PASSWORD"),
-    "database": "defaultdb",
-    "port": 23912,
-    "ssl_ca": "config/ca.pem"
+    "host": "localhost",
+    "user": "root",
+    "password": "",
+    "database": "odontoprodb",
+    "port": 3306
 }
