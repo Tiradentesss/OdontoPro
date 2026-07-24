29c896580b1b578379af2a34bb689bdbb442ef40
Celio <celio56430576@edu.pa.senac.br>
2026-07-24 16:24:14 -0300

    arrumar fundo

--- ODTPlace/src/screens/CheckEmail.js
+++ ODTPlace/src/screens/CheckEmail.js
@@ -108,8 +108,10 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     position: 'absolute',
-    width: '100%',
-    height: '100%',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
   },
   headerRow: {
     marginTop: 32,
