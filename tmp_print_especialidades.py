8c3a741c5f3a34e303904101c59b2d8079f1241f
Lucaspc01 <Lucas_pc01@hotmail.com>
2026-08-16 01:16:42 -0300

    config_ok_4

--- tmp_print_especialidades.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, r'c:\Users\58143406\Documents\Desktop_2\OdontoPro\SistemaDesktop')
-from controllers.consulta_controller import ConsultaController
-print(ConsultaController.listar_especialidades_para_combo(1))
