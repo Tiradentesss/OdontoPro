5db50170b4cd0e12d71d6147c90b705b4835b6c2
Celioss <odontopro2@gmail.com>
2026-08-21 11:29:01 -0300

    update

--- ODTPlace/src/screens/ProfessionalInfoScreen.js
+++ ODTPlace/src/screens/ProfessionalInfoScreen.js
@@ -162,6 +162,7 @@ const styles = StyleSheet.create({
     width: 120,
     height: 120,
     borderRadius: 60,
+    overflow: 'hidden',
     backgroundColor: '#eef8ff',
     alignItems: 'center',
     justifyContent: 'center',
