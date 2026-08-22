c0d9b555b4dbec8e42ae1de325c35ee6da3f8efc
Celioss <odontopro2@gmail.com>
2026-08-22 16:19:39 -0300

    mais updates

--- odontoPro/static/js/DashboardPaciente/dashboard.js
+++ odontoPro/static/js/DashboardPaciente/dashboard.js
@@ -769,8 +769,8 @@ function abrirModalAgendamento(clinicaId) {
                     this.src = "/static/img/sem-foto-de-perfil.jpg";
                 };
 
-                if (data.banner_url) {
-                    logoImg.src = data.banner_url;
+                if (data.logo_url) {
+                    logoImg.src = data.logo_url;
                 } else {
                     logoImg.src = "/static/img/sem-foto-de-perfil.jpg";
                 }
